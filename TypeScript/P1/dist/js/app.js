9a23ce420c6445fbd5b037cbbc7e96b74ff1966f
Henrique Morais <rikeaju@hotmail.com>
2022-04-05 12:04:53 -0300

    Lesson 8
    Gets and a explanation about types.

--- TypeScript/P1/dist/js/app.js
+++ TypeScript/P1/dist/js/app.js
@@ -1,6 +1,3 @@
 import { Negotiation } from "./models/negotiation.js";
-
 const negotiation = new Negotiation(new Date(), 10, 100);
-console.log(negotiation);
-negotiation.quantity = 1000;
-console.log(negotiation);
+console.log(negotiation.volume);
--- TypeScript/P1/dist/js/models/negotiation.js
+++ TypeScript/P1/dist/js/models/negotiation.js
@@ -8,4 +8,20 @@ export class Negotiation {
         this.#quantity = quantity;
         this.#value = value;
     }
+
+    get date() {
+        return this.#date;
+    }
+
+    get quantity() {
+        return this.#quantity;
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    get volume() {
+        return this.#quantity * this.#value;
+    }
 }
