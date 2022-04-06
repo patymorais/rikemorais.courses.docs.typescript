f170d53a4126614563a9458b3f0430f3e254df61
Henrique Morais <rikeaju@hotmail.com>
2022-04-06 16:59:56 -0300

    noEmitOnError
    Prevent the TypeScript compiler from managing JavaScript files with error.

--- TypeScript/P1/app/app.ts
+++ TypeScript/P1/app/app.ts
@@ -1,3 +1,3 @@
 import { Negotiation } from "./models/negotiation.js";
-const negotiation = new Negotiation(new Date());
+const negotiation = new Negotiation(new Date(), 10, 100);
 console.log(negotiation.volume);
