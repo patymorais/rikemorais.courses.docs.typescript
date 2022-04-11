46744e5f09a82a50333b0d42b280d8773da1136b
Henrique Morais <rikeaju@hotmail.com>
2022-04-11 12:24:28 -0300

    Final P1

--- TypeScript/P1/app/models/negotiation.ts
+++ TypeScript/P1/app/models/negotiation.ts
@@ -1,12 +1,17 @@
 export class Negotiation {
 
     constructor(
-         public readonly date: Date, 
+         private _date: Date, 
          public readonly quantity: number, 
          public readonly value: number
     ) {}
 
     get volume(): number {
         return this.quantity * this.value;
+    }
+
+    get date(): Date {
+        const date = new Date(this._date.getTime());
+        return date;
     }
 }
