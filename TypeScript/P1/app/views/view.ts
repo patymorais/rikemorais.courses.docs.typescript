066edd8fc90e969f9857625951243bd97411644f
Henrique Morais <rikeaju@hotmail.com>
2022-04-15 18:39:31 -0300

    Lesson 13

--- TypeScript/P1/app/views/view.ts
+++ TypeScript/P1/app/views/view.ts
@@ -1,13 +1,11 @@
-export class View<T> {
+export abstract class View<T> {
     protected element: HTMLElement;
 
     constructor(selector: string) {
         this.element = document.querySelector(selector);
     }
 
-    template(model: T): string {
-        throw new Error('Method not implemented.');
-    }
+    abstract template(model: T): string;
 
     update(model: T): void {
         const template = this.template(model);
--- TypeScript/P1/dist/js/views/view.js
+++ TypeScript/P1/dist/js/views/view.js
@@ -2,9 +2,6 @@ export class View {
     constructor(selector) {
         this.element = document.querySelector(selector);
     }
-    template(model) {
-        throw new Error('Method not implemented.');
-    }
     update(model) {
         const template = this.template(model);
         this.element.innerHTML = template;
