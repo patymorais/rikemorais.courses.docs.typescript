36ef3bbe0b9a3d91b60ac3dce2ee713334065579
Henrique Morais <rikeaju@hotmail.com>
2022-04-18 11:48:30 -0300

    Lesson 05

--- TypeScript/P1/app/dist/js/decorators/timeLog.js
+++ TypeScript/P1/app/dist/js/decorators/timeLog.js
@@ -1,5 +1,13 @@
 export function timeLog() {
     return function (target, propertyKey, descriptor) {
+        const originalMethod = descriptor.value;
+        descriptor.value = function () {
+            const t1 = performance.now();
+            const returning = originalMethod();
+            const t2 = performance.now();
+            console.log(`${target.constructor.name} ${propertyKey} took ${t2 - t1} ms.`);
+            returning;
+        };
         return descriptor;
     };
 }
--- TypeScript/P1/app/src/decorators/timeLog.ts
+++ TypeScript/P1/app/src/decorators/timeLog.ts
@@ -3,6 +3,14 @@ export function timeLog() {
         target: any, 
         propertyKey: string, 
         descriptor: PropertyDescriptor) {
-            return descriptor;
+        const originalMethod = descriptor.value;
+        descriptor.value = function () {
+            const t1 = performance.now();
+            const returning = originalMethod();
+            const t2 = performance.now();
+            console.log(`${target.constructor.name} ${propertyKey} took ${t2 - t1} ms.`);
+            returning;
+        }
+        return descriptor;
     }
 }
