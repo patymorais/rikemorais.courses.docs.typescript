709c6e152796f0ff2a8e82257f405b53221125b6
Henrique Morais <rikeaju@hotmail.com>
2022-04-18 17:00:06 -0300

    Lesson 08

--- TypeScript/P1/app/dist/js/decorators/timeLog.js
+++ TypeScript/P1/app/dist/js/decorators/timeLog.js
@@ -1,11 +1,17 @@
-export function timeLog() {
+export function timeLog(inSeconds = false) {
     return function (target, propertyKey, descriptor) {
         const originalMethod = descriptor.value;
         descriptor.value = function (...args) {
+            let divide = 1;
+            let unit = 'ms';
+            if (inSeconds) {
+                divide = 1000;
+                unit = 's';
+            }
             const t1 = performance.now();
             const returning = originalMethod.apply(this, args);
             const t2 = performance.now();
-            console.log(`${target.constructor.name} ${propertyKey} took ${t2 - t1} ms.`);
+            console.log(`${target.constructor.name} ${propertyKey} took ${(t2 - t1) / divide} ${unit}}.`);
             returning;
         };
         return descriptor;
--- TypeScript/P1/app/src/decorators/timeLog.ts
+++ TypeScript/P1/app/src/decorators/timeLog.ts
@@ -1,14 +1,20 @@
-export function timeLog() {
+export function timeLog(inSeconds: boolean = false) {
     return function (
         target: any, 
         propertyKey: string, 
         descriptor: PropertyDescriptor) {
         const originalMethod = descriptor.value;
         descriptor.value = function (...args: any[]) {
+            let divide = 1;
+            let unit = 'ms';
+            if (inSeconds) {
+                divide = 1000;
+                unit = 's';
+            }
             const t1 = performance.now();
             const returning = originalMethod.apply(this, args);
             const t2 = performance.now();
-            console.log(`${target.constructor.name} ${propertyKey} took ${t2 - t1} ms.`);
+            console.log(`${target.constructor.name} ${propertyKey} took ${(t2 - t1)/divide} ${unit}}.`);
             returning;
         }
         return descriptor;
